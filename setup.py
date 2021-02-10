c844ac1124b4fa62795d82ab9f03f14e5f1bdc3e
ca-chow <cc5718@nyu.edu>
2021-02-10 13:35:04 -0500

    Added stub software library, see #2

---
+++ setup.py
@@ -0,0 +1,13 @@
+#Setting up pi settings for our robot companion
+
+from setuptools import setup
+
+setup(name='virtualhat',
+	version='1.0',
+	description='CompE Stub Library',
+	author='Cassandra Chow',
+	author_email='cc5718@nyu.edu',
+	url='https://github.com/ffund/virtualhat',
+	py_modules=['virtualhat'],
+	)
+
