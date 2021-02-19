2b46e87590eeb03cee70ac586242467fb3daab5a
qyc206 <qyc206@nyu.edu>
2021-02-18 23:38:21 -0500

    Added stub function, see #3

---
+++ demo.py
@@ -0,0 +1,19 @@
+from stubby_eui import *
+
+info()
+
+turnOnLED()
+turnOffLED()
+changeLEDColor()
+sendByte()
+
+makeSound()
+makeMelody()
+
+motorTest()
+rightTurn()
+leftTurn()
+
+readDist()
+registerTap()
+buttonPressed()
--- stubby_eui.py
+++ stubby_eui.py
@@ -2,3 +2,48 @@ def info():
 	'''Prints a basic library description'''
 	print("Software library for Eui.")
 
+
+## lights ##
+
+def turnOnLED():
+	print("LEDs turn on")
+
+def turnOffLED():
+	print("LEDs turn off")
+
+def changeLEDColor():
+	print("LEDs change color")
+
+def sendByte():
+	print("send byte to shift register")
+
+## sound ##
+
+def makeSound():
+	print("piezo make a sound")
+
+def makeMelody():
+	print("piezo plays a tone")
+
+## movements ##
+
+def motorTest():
+	print("power motors on")
+
+def rightTurn():
+	print("make right turn")
+
+def leftTurn():
+	print("make left turn")
+
+## sensors ##
+
+def readDist():
+	print("distance read by sensor")
+
+def registerTap():
+	print("gets a signal from linear softpot")
+
+def buttonPressed():
+	print("button is pressed")
+
