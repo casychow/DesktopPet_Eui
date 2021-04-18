2890ae420ccf7027b49f01e2885ae2cfc1990edb
Qin Ying Chen <qinyingchen@qycs-mbp.myfiosgateway.com>
2021-04-18 16:51:56 -0400

    can run code to display webpage, see #17

--- eui_UI/eui.py
+++ eui_UI/eui.py
@@ -2,10 +2,26 @@
 from flask import Flask, redirect, render_template
 app = Flask(__name__, static_folder='')
 
-# Define a route to hello function
+# Load pages 
 @app.route("/")
-def hello():
+def loadHome():
     return render_template('index.html')
+
+@app.route("/pomodoro")
+def loadPomodoro():
+    return render_template('pomodoro.html')
+
+@app.route("/statistic")
+def loadStatistic():
+    return render_template('statistic.html')
+
+@app.route("/alert")
+def loadAlert():
+    return render_template('alert.html')
+
+@app.route("/contact_us")
+def loadContactUs():
+    return render_template('contact_us.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
