6234baab6c3f1f350c21b2e8c342b61ac0e16e15
Qin Ying Chen <qinyingchen@qycs-MBP.fios-router.home>
2021-04-28 14:51:55 -0400

    updated browser to store user configurations, see #17

--- eui_UI/eui.py
+++ eui_UI/eui.py
@@ -1,25 +1,32 @@
 import datetime
+import yaml     # for storing user information
+
 # Import Flask Library
 from flask import Flask, redirect, render_template, request
 app = Flask(__name__, static_folder='assets')
 
-# Globals (these will all be stored in database)
-USERNAME = "Buddy"
+# Default user settings (as stored in "usersettings_default.p")
+DEFAULT_USER_SETTINGS = { "username" : "Buddy",
+                            "workPeriod" : 25,    # in minutes
+                            "restPeriod" : 5,     # in minutes
+                            "numReminder" : 0,
+                            "numSnooze" : 2,
+                            "workOption" : 1,
+                            "workPersonalized" : "",
+                            "restOption" : 1,
+                            "restPersonalized" : "",
+                            "soundOption" : 1,
+                            "motionOption" : 1,
+                            "lightOption" : 1
+                        }
+                        
+# Grab stored user settings
+with open('usersettings.yaml') as file:
+    # The FullLoader parameter handles the conversion from YAML
+    # scalar values to Python the dictionary format
+    USER_SETTINGS = yaml.load(file, Loader=yaml.FullLoader)
 
-workPeriod = 25
-restPeriod = 5
-numReminder = 2 
-numSnooze = 0
-
-workOption = 1
-restOption = 1
-workPersonalized = ""
-restPersonalized = ""
-
-soundOption = 1
-motionOption = 1
-lightOption = 1
-
+# Globals
 streak_numOfDays = 0
 streak_pomodorosOneDayAmt = 0
 streak_pomodorosOneDayDate = datetime.datetime.now().strftime("%m/%d/%Y")
@@ -28,29 +35,34 @@ streak_pomodorosOneDayDate = datetime.datetime.now().strftime("%m/%d/%Y")
 
 @app.route("/")
 def loadHome():
-    return render_template('index.html', username=USERNAME)
+    return render_template('index.html', username=USER_SETTINGS["username"])
 
 @app.route('/editname', methods=['GET', 'POST'])
 def editName():
     username = request.form['username']
 
     if (username != ""):
-        global USERNAME
-        USERNAME = username
+        global USER_SETTINGS
+        USER_SETTINGS['username'] = username
 
-    return render_template('index.html', username=USERNAME)
+        # update yaml data
+        with open('usersettings.yaml', 'w') as file:
+            yaml.dump(USER_SETTINGS, file)
+
+    return render_template('index.html', username=USER_SETTINGS['username'])
 
 # Handle Pomodoro page
 
 @app.route("/pomodoro")
 def loadPomodoro():
-    return render_template('pomodoro.html', workOption=workOption, restOption=restOption, 
-                            workPeriod=workPeriod, restPeriod=restPeriod, 
-                            numReminder=numReminder, numSnooze=numSnooze, 
-                            setWorkPersonalized=workPersonalized, setRestPersonalized=restPersonalized)
+    return render_template('pomodoro.html', workOption=USER_SETTINGS['workOption'], restOption=USER_SETTINGS['restOption'], 
+                            workPeriod=USER_SETTINGS['workPeriod'], restPeriod=USER_SETTINGS['restPeriod'], 
+                            numReminder=USER_SETTINGS['numReminder'], numSnooze=USER_SETTINGS['numSnooze'], 
+                            setWorkPersonalized=USER_SETTINGS['workPersonalized'], setRestPersonalized=USER_SETTINGS['restPersonalized'])
 
 @app.route("/editpomodorotimer", methods=['GET', 'POST'])
 def editPomodoroTimer():
+    global USER_SETTINGS
 
     ''' Retreive Timing Fields '''
 
@@ -58,60 +70,56 @@ def editPomodoroTimer():
     newRestPeriod = request.form['newRestPeriod']
 
     if (newWorkPeriod != ""):
-        global workPeriod
-        workPeriod = newWorkPeriod
+        USER_SETTINGS['workPeriod'] = int(newWorkPeriod)
     if (newRestPeriod != ""):
-        global restPeriod
-        restPeriod = newRestPeriod
+        USER_SETTINGS['restPeriod'] = int(newRestPeriod)
 
     newNumReminder = request.form['newNumReminder']
     newNumSnooze = request.form['newNumSnooze']
 
     if (newNumReminder != ""):
-        global numReminder
-        numReminder = newNumReminder
+        USER_SETTINGS['numReminder'] = int(newNumReminder)
     if (newNumSnooze != ""):
-        global numSnooze
-        numSnooze = newNumSnooze
+        USER_SETTINGS['numSnooze'] = int(newNumSnooze)
 
     ''' Retreive Accountability Fields '''
 
-    global workPersonalized
-    global workOption
     getOption = request.form['workOptions']
 
     if (getOption == "work_option1"):
-        workOption = 1
-        workPersonalized = ""
+        USER_SETTINGS['workOption'] = 1
+        USER_SETTINGS['workPersonalized'] = ""
     elif (getOption == "work_option2"):
-        workOption = 2
-        workPersonalized = ""
+        USER_SETTINGS['workOption'] = 2
+        USER_SETTINGS['workPersonalized'] = ""
     else:
-        workOption = 3
+        USER_SETTINGS['workOption'] = 3
         getVal = request.form['workPersonalized']
         if (getVal != ""):
-            workPersonalized = getVal
+            USER_SETTINGS['workPersonalized'] = getVal
 
-    global restPersonalized
-    global restOption
     getOption = request.form['restOptions']
 
     if (getOption == "rest_option1"):
-        restOption = 1
-        restPersonalized = ""
+        USER_SETTINGS['restOption'] = 1
+        USER_SETTINGS['restPersonalized'] = ""
     elif (getOption == "rest_option2"):
-        restOption = 2
-        restPersonalized = ""
+        USER_SETTINGS['restOption'] = 2
+        USER_SETTINGS['restPersonalized'] = ""
     else:
-        restOption = 3
+        USER_SETTINGS['restOption'] = 3
         getVal = request.form['restPersonalized']
         if (getVal != ""):
-            restPersonalized = getVal
+            USER_SETTINGS['restPersonalized'] = getVal
 
-    return render_template('pomodoro.html', workOption=workOption, restOption=restOption, 
-                            workPeriod=workPeriod, restPeriod=restPeriod, 
-                            numReminder=numReminder, numSnooze=numSnooze, 
-                            setWorkPersonalized=workPersonalized, setRestPersonalized=restPersonalized)
+    # update yaml data
+    with open('usersettings.yaml', 'w') as file:
+        yaml.dump(USER_SETTINGS, file)
+
+    return render_template('pomodoro.html', workOption=USER_SETTINGS['workOption'], restOption=USER_SETTINGS['restOption'], 
+                            workPeriod=USER_SETTINGS['workPeriod'], restPeriod=USER_SETTINGS['restPeriod'], 
+                            numReminder=USER_SETTINGS['numReminder'], numSnooze=USER_SETTINGS['numSnooze'], 
+                            setWorkPersonalized=USER_SETTINGS['workPersonalized'], setRestPersonalized=USER_SETTINGS['restPersonalized'])
 
 # Handle Statistics page
 
@@ -124,58 +132,62 @@ def loadStatistic():
 
 @app.route("/alert")
 def loadAlert():
-    return render_template('alert.html', soundOption=soundOption, motionOption=motionOption, lightOption=lightOption)
+    return render_template('alert.html', soundOption=USER_SETTINGS['soundOption'], 
+                            motionOption=USER_SETTINGS['motionOption'], lightOption=USER_SETTINGS['lightOption'])
 
 @app.route("/editalert", methods=['GET', 'POST'])
 def editAlert():
+    global USER_SETTINGS
+
     getOption = request.form['soundOptions']
-    global soundOption
 
     if (getOption == "sound_option1"):
-        soundOption = 1
+        USER_SETTINGS['soundOption'] = 1
     elif (getOption == "sound_option2"):
-        soundOption = 2
+        USER_SETTINGS['soundOption'] = 2
     elif (getOption == "sound_option3"):
-        soundOption = 3
+        USER_SETTINGS['soundOption'] = 3
     elif (getOption == "sound_option4"):
-        soundOption = 4
+        USER_SETTINGS['soundOption'] = 4
 
     getOption = request.form['motionOptions']
-    global motionOption
 
     if (getOption == "motion_option1"):
-        motionOption = 1
+        USER_SETTINGS['motionOption'] = 1
     elif (getOption == "motion_option2"):
-        motionOption = 2
+        USER_SETTINGS['motionOption'] = 2
     elif (getOption == "motion_option3"):
-        motionOption = 3
+        USER_SETTINGS['motionOption'] = 3
     elif (getOption == "motion_option4"):
-        motionOption = 4
+        USER_SETTINGS['motionOption'] = 4
     elif (getOption == "motion_option5"):
-        motionOption = 5
+        USER_SETTINGS['motionOption'] = 5
 
     getOption = request.form['lightOptions']
-    global lightOption
 
     if (getOption == "light_option1"):
-        lightOption = 1
+        USER_SETTINGS['lightOption'] = 1
     elif (getOption == "light_option2"):
-        lightOption = 2
+        USER_SETTINGS['lightOption'] = 2
     elif (getOption == "light_option3"):
-        lightOption = 3
+        USER_SETTINGS['lightOption'] = 3
     elif (getOption == "light_option4"):
-        lightOption = 4
+        USER_SETTINGS['lightOption'] = 4
     elif (getOption == "light_option5"):
-        lightOption = 5
+        USER_SETTINGS['lightOption'] = 5
 
-    return render_template('alert.html', soundOption=soundOption, motionOption=motionOption, lightOption=lightOption)
+    # update yaml data
+    with open('usersettings.yaml', 'w') as file:
+        yaml.dump(USER_SETTINGS, file)
+
+    return render_template('alert.html', soundOption=USER_SETTINGS['soundOption'], 
+                            motionOption=USER_SETTINGS['motionOption'], lightOption=USER_SETTINGS['lightOption'])
 
 # Load Contacts page
 
 @app.route("/contact_us")
 def loadContactUs():
     return render_template('contact_us.html')
-
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
-
